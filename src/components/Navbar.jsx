7e1b6c8f61eddd0f2f73248583d2e7946aecc6af
Skand Tiwari <65547970+SkandTiwari@users.noreply.github.com>
2022-07-10 22:28:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 import {Button, Menu, Typography, Avatar} from 'antd';
 import {Link} from 'react-router-dom';
 import{HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons';
